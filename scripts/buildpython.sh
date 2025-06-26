fe38293d58bf35b8bbac70bcd7694391835486f0
Ashwin Naren <arihant2math@gmail.com>
2025-06-26 12:02:25 -0700

    fixes

--- scripts/buildpython.sh
+++ scripts/buildpython.sh
@@ -1,1 +1,1 @@
-toolforge jobs run bootstrap-venv --command "cd $PWD && ./graphbot/scripts/bootstrappython.sh" --image python3.11
+toolforge jobs run bootstrap-venv --command "cd $PWD && ./scripts/bootstrappython.sh" --image python3.11

7e7c119cb2ba0757c1fe8a211d4b30ed61a87d9a
Ashwin Naren <arihant2math@gmail.com>
2025-06-26 12:11:19 -0700

    renaming scripts

---
+++ scripts/build_python.sh
@@ -0,0 +1,1 @@
+toolforge jobs run bootstrap-venv --command "cd $PWD && ls scripts && sh ./scripts/bootstrap_python.sh" --image python3.11
--- scripts/buildpython.sh
+++
@@ -1,1 +0,0 @@
-toolforge jobs run bootstrap-venv --command "cd $PWD && ls && pwd && sh ./scripts/bootstrappython.sh" --image python3.11

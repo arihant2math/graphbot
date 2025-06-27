88a5f29bcf36c9cb90378819d7fc4d2340352965
Ashwin Naren <arihant2math@gmail.com>
2025-06-26 20:25:46 -0700

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 toolforge jobs delete bot
 mkdir -p ~/bin/
 cp ./target/release/graphbot ~/bin/
-toolforge jobs run bot --command ~/graphbot --image python3.11 --continuous
+toolforge jobs run bot --command ~/bin/graphbot --image python3.11 --continuous

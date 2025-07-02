726a695b47026f84c34b2a5f953940a875b4d8f9
Ashwin Naren <arihant2math@gmail.com>
2025-07-02 14:51:07 -0700

    improve rfd task

--- src/rfd_task/mod.rs
+++ src/rfd_task/mod.rs
@@ -6,7 +6,7 @@ use tracing::info;
 use crate::config::Config;
 use crate::parser;
 
-const RFD_PAGE: &str = "Wikipedia:Redirects for discussion";
+const MAIN_RFD_PAGE: &str = "Wikipedia:Redirects for discussion";
 
 pub async fn rfd_task(
     wiki_bot: Arc<Bot>,
@@ -21,7 +21,7 @@ pub async fn rfd_task(
             tokio::time::sleep(Duration::from_secs(1)).await;
             continue;
         }
-        let page = wiki_bot.page(RFD_PAGE)?;
+        let page = wiki_bot.page(MAIN_RFD_PAGE)?;
         let parsed = parser::call_parser(&page.wikitext().await?, &config).await?;
         let mut rfd_pages = Vec::with_capacity(10);
         for template in parsed.parsed.templates {
@@ -34,6 +34,72 @@ pub async fn rfd_task(
             let rfd_page_obj = wiki_bot.page(&rfd_page)?;
             let rfd_text = rfd_page_obj.wikitext().await?;
             let parsed_rfd = parser::call_parser(&rfd_text, &config).await?;
+
+            #[derive(Clone, Debug, Default)]
+            struct Rfd {
+                name: String,
+                associated_redirects: Vec<String>,
+            }
+
+            struct State {
+                rfds_started: bool,
+                rfds: Vec<Rfd>,
+            }
+
+            impl State {
+                fn new() -> Self {
+                    State {
+                        rfds_started: false,
+                        rfds: Vec::new(),
+                    }
+                }
+
+                fn start_rfds(&mut self) {
+                    assert!(!self.rfds_started, "RFDS already started, cannot start again.");
+                    self.rfds_started = true;
+                    assert!(self.rfds.is_empty(), "RFDS already non-zero length.");
+                    self.rfds.push(Rfd::default());
+                }
+
+                fn current_rfd(&self) -> &Rfd {
+                    self.rfds.last().unwrap()
+                }
+
+                fn current_rfd_mut(&mut self) -> &mut Rfd {
+                    self.rfds.last_mut().unwrap()
+                }
+
+                fn finalize_rfd(&mut self) {
+                    assert!(self.rfds_started, "RFDS not started, cannot finalize.");
+                    assert!(!self.current_rfd().name.is_empty(), "RFD name is empty, cannot finalize.");
+                    assert!(!self.current_rfd().associated_redirects.is_empty(), "RFD has no associated redirects, cannot finalize.");
+                    self.rfds.push(Rfd::default());
+                }
+            }
+
+            let mut state = State::new();
+
+            for node in &parsed_rfd.parsed.nodes {
+                if let Some(comment) = node.clone().into_comment() {
+                    if comment.text.contains("Add new entries directly below this line") {
+                        state.start_rfds();
+                        continue; // Skip the comment line
+                    }
+                }
+                if state.rfds_started {
+                    // dbg!(&node.text);
+                    if let Some(heading) = node.clone().into_heading() {
+                        if heading.level() == 4 {
+                            state.current_rfd_mut().name = heading.title_str().to_string().trim().to_string();
+                        }
+                    } else {
+                        dbg!(&node.text);
+                        if let Some(span) = node.clone().into_html_entity() {
+                            dbg!(span);
+                        }
+                    }
+                }
+            }
         }
         tokio::time::sleep(Duration::from_secs(1)).await;
     }

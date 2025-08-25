e4fd665a01064fa71707b1e7dc212ed4532587f9
Ashwin Naren <arihant2math@gmail.com>
2025-08-24 20:22:21 -0700

    add some logging to deploy process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,5 @@
+echo "Deleting old job and logs..."
 toolforge jobs delete bot
+rm ~/bot.out ~/bot.err
+echo "Deploying new job..."
 toolforge jobs run bot --command "bash ~/graphbot/scripts/run.bash" --image python3.11 --continuous --cpu 3 --mem 2G

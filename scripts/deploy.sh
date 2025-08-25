2155b11120c5d0dea496e764b9ceec4dbcf08303
Ashwin Naren <arihant2math@gmail.com>
2025-08-24 20:19:12 -0700

    fix broken deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,2 @@
 toolforge jobs delete bot
-mkdir -p ~/bin/
-cp ./target/release/graphbot ~/bin/
 toolforge jobs run bot --command "bash ~/graphbot/scripts/run.bash" --image python3.11 --continuous --cpu 3 --mem 2G
--- scripts/run.bash
+++ scripts/run.bash
@@ -4,4 +4,6 @@ cd ~/graphbot
 source .venv/bin/activate
 python3 server.py &
 cargo build -r
+mkdir -p ~/bin/
+cp ./target/release/graphbot ~/bin/
 ./target/release/graphbot

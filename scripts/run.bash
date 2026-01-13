63d89310a72eb2690a21a835c689ca89cb530f5e
Ashwin Naren <arihant2math@gmail.com>
2026-01-12 21:31:14 -0800

    updates

--- scripts/run.bash
+++ scripts/run.bash
@@ -3,7 +3,8 @@ source ~/.profile
 cd ~/graphbot
 source .venv/bin/activate
 python3 server.py &
-cargo build -r
+cargo build --release -p frontend
+cargo build --release
 mkdir -p ~/bin/
 cp ./target/release/graphbot ~/bin/
 ./target/release/graphbot
